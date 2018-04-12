6b755fc5f355fe92239725d37f4054b6d05c63f2
Keyur Gajjar <kga@odoo.com>
2018-04-12 10:21:01 -0700

    [ADD] deliery report

--- monivertical_report_customization/__manifest__.py
+++ monivertical_report_customization/__manifest__.py
@@ -6,7 +6,7 @@ Monivertical Report Customization
 =================================
 - Sale Report Customization
     """,
-    'depends': ['web', 'sale_management',  'point_of_sale'],
+    'depends': ['web', 'sale_management',  'point_of_sale', 'stock', 'fleet', 'delivery'],
     'data': [
             # Report
             'report/classic_report_template.xml',
@@ -16,8 +16,11 @@ Monivertical Report Customization
             'report/fxo_custom_report_sale_order.xml',
             'report/saleorder_report_inherit.xml',
             'report/report.xml',
+            'report/report_delivery_slip_custom.xml',
             # views
+            'views/stock_picking_views.xml',
             'views/res_company_views.xml',
+            'views/delivery_carrier_views.xml'
         ],
     'qweb': [],
 
--- monivertical_report_customization/models/sale_order.py
+++ monivertical_report_customization/models/sale_order.py
@@ -10,9 +10,3 @@ class SaleOrder(models.Model):
 	print_image = fields.Boolean(string="Print Image", help="If ticked, you can see the product image in report of sale order/quotation", default=True)
 	image_sizes = fields.Selection([('image', "Big Size Image"), ('image_small', "Small Size Image"), ('image_medium', "Medium Size Image")], 
 		string='Image Size', default='image_medium', help="Image size to be displayed in report")
-
-
-class SaleOrderLine(models.Model):
-	_inherit = 'sale.order.line'
-
-	list_price = fields.Float(related="product_id.list_price")
